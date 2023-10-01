784c7544e9d5845b6ab2dad86b75e79b9603b8c8
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-10-01 00:31:57 -0700

    Add rudimentary screen capture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
+'use client'
+
+import { useRef } from "react";
+
 export default function Home() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  
+  async function startCapture() {
+    try {
+      if (videoRef.current) {
+        videoRef.current.srcObject =
+          await navigator.mediaDevices.getDisplayMedia({
+            video: {
+              displaySurface: "window",
+            },
+            audio: false,
+          });
+      }
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+  }
+
+  function stopCapture() {
+    if (videoRef.current && videoRef.current.srcObject) {
+      (videoRef.current.srcObject as MediaStream).getTracks().forEach(track => track.stop());
+    }
+  }
+  
   return (
     <main>
       Hello world!
+      <button onClick={startCapture}>Start Capture</button>
+      <button onClick={stopCapture}>Stop Capture</button>
+      <video id="video" ref={videoRef} autoPlay></video>
     </main>
   )
 }
