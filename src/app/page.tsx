dce42512d21f423d1cadf6466554fa8bb0973cc9
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-10-01 21:32:50 -0700

    Run prettier on files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
-'use client'
+'use client';
 
-import { useRef } from "react";
-import { cavernRelics } from "./relics";
+import { useRef } from 'react';
+import { cavernRelics } from './relics';
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
-  
+
   async function startCapture() {
     try {
       if (videoRef.current) {
-        videoRef.current.srcObject =
-          await navigator.mediaDevices.getDisplayMedia({
-            video: {
-              displaySurface: "window",
-            },
-            audio: false,
-          });
+        videoRef.current.srcObject = await navigator.mediaDevices.getDisplayMedia({
+          video: {
+            displaySurface: 'window',
+          },
+          audio: false,
+        });
       }
     } catch (err) {
       console.error(`Error: ${err}`);
@@ -24,19 +23,25 @@ export default function Home() {
 
   function stopCapture() {
     if (videoRef.current && videoRef.current.srcObject) {
-      (videoRef.current.srcObject as MediaStream).getTracks().forEach(track => track.stop());
+      (videoRef.current.srcObject as MediaStream).getTracks().forEach((track) => track.stop());
     }
   }
-  
+
   return (
     <main>
       <h1>Honkai Star Rail Relic Scorer</h1>
-      <button className="btn btn-blue" onClick={startCapture}>Start Capture</button>
-      <button className="btn btn-blue" onClick={stopCapture}>Stop Capture</button>
-      <video className="video" ref={videoRef} autoPlay></video>
-      <select name="cavernRelics" defaultValue={cavernRelics[0].name}>
-        {cavernRelics.map(relic => <option value={relic.name}>{relic.name}</option>)}
+      <button className='btn btn-blue' onClick={startCapture}>
+        Start Capture
+      </button>
+      <button className='btn btn-blue' onClick={stopCapture}>
+        Stop Capture
+      </button>
+      <video className='video' ref={videoRef} autoPlay></video>
+      <select name='cavernRelics' defaultValue={cavernRelics[0].name}>
+        {cavernRelics.map((relic) => (
+          <option value={relic.name}>{relic.name}</option>
+        ))}
       </select>
     </main>
-  )
+  );
 }
